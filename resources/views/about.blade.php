c26876960e654c8bbb7fef3d4e88308731177351
setoaryotomo <seto4137@gmail.com>
2024-05-22 18:28:19 +0700

    Blade Component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <h1>Halaman About</h1>
-    <h3>Owner: {{ $nama }}</h3>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman About</h3>
+    <p>Nama : {{ $name }}</p>
+</x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-</head>
-<body>
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li>
-            <a href="">Instagram</a>
-        </li>
-        <li>
-            <a href="">Youtube</a>
-        </li>
-    </ul>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman Contact</h3>
+</x-layout>
