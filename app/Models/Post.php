9333911d23a60f5dbbe4b01bcb474f4b2790070e
setoaryotomo <seto4137@gmail.com>
2024-05-24 17:47:41 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Seto Aryotomo',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe delectus fuga, iusto adipisci totam rerum porro temporibus ad fugiat at perferendis, dicta placeat et blanditiis ea! Temporibus maiores nobis beatae!'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Seto Aryotomo',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe delectus fuga, iusto adipisci totam rerum porro temporibus ad fugiat at perferendis, dicta placeat et blanditiis ea! Temporibus maiores nobis beatae!'
-            ]
-        ];
-    }
-    public static function find($slug): array 
-    {
-
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-        if (! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title','author','slug','body'];
 }
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
+
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Posts', 'posts' => Post::all()]);
 });
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
@@ -20,10 +22,8 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Seto'], ['title' => 'About']);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
+Route::get('/posts/{post:slug}', function (Post $post) {
 
-
-    $post = Post::find($slug);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
